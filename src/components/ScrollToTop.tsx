af13cd03145f490809b54846ffb0c0c03625a241
Akshat Kumar <akshat1028kumar@gmail.com>
2026-03-06 23:53:29 +0530

    Added scroll to top

---
+++ src/components/ScrollToTop.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { useState, useEffect } from "react";
+import { ArrowUp } from "lucide-react";
+
+const ScrollToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
+  return (
+    <>
+      {isVisible && (
+        <button
+          onClick={scrollToTop}
+          className="fixed cursor-pointer bottom-6 right-6 z-50 p-2 rounded-full bg-blue-600 text-white shadow-xl hover:bg-blue-700 transition transform hover:-translate-y-1 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+          aria-label="Scroll to top"
+        >
+          <ArrowUp size={24} />
+        </button>
+      )}
+    </>
+  );
+};
+
+export default ScrollToTop;
